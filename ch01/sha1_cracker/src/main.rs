77d9608820e59c5b7984d9b1ff96f167e7f24591
0xw1ld <w1ld@0xw1ld.github.io>
2026-01-12 18:16:03 +1100

    [ch01] Sha1 Cracker

--- ch01/sha1_cracker/src/main.rs
+++ ch01/sha1_cracker/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use sha1::Digest;
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead,BufReader},
+};
+
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("{} <wordlist.txt> <sha1_hash>",args[0]);
+        return Ok(());
+    }
+
+    let hash = args[2].trim();
+    if hash.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("Sha1 hash is not valid".into());
+    }
+
+    let wordlist: File = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist);
+
+    for line in reader.lines() {
+        let line = line?;
+        let password = line.trim();
+
+        if hash == &hex::encode(sha1::Sha1::digest(password.as_bytes())){
+            println!("Password found: {}",&password);
+            return Ok(());
+        }
+    }
+
+    println!("Password not found in wordlist.");
+
+    Ok(())
 }
